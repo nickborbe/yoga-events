5298953b2a3b486fd495d30939008e7275b1a050
nickborbe <nickborbe@gmail.com>
2019-01-16 10:20:19 -0500

    events controller get index and details routes

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Event  = require('../models/Event');
 
 /* GET home page */
 router.get('/events', (req, res, next) => {
-//   res.json('events');
+    Event.find()
+    .then((allTheEvents)=>{
+        res.json(allTheEvents)
+    })
+    .catch((err)=>{
+        res.json(err.message);
+    })
 });
+
+
+
+
+router.get('/events/:id', (req, res, next)=>{
+    Event.findById(req.params.id)
+    .then((theEvent)=>{
+        res.json(theEvent)
+    })
+    .catch((err)=>{
+        res.json(err.message);
+    })
+
+
+})
+
 
 module.exports = router;
